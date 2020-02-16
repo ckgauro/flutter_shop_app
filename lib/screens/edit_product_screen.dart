1e3dc64c4a127d7ea708b2ce25c95b52065ad5cc
Chandra kishor Gauro <ckgauro@gmail.com>
2020-02-16 12:39:07 -0500

    222

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
-  static const routeName='/edit-product';
+  static const routeName = '/edit-product';
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
-  final _priceFocusNode=FocusNode();
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
 
-  
+  @override
+  void initState() {
+    // TODO: implement initState
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +49,11 @@ class _EditProductScreenState extends State<EditProductScreen> {
             TextFormField(
               decoration: InputDecoration(labelText: 'Title'),
               textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_){
+              onFieldSubmitted: (_) {
+                print(
+                  _imageUrlController.text,
+                );
+                print(_imageUrlController.text.isEmpty);
                 FocusScope.of(context).requestFocus(_priceFocusNode);
               },
             ),
@@ -33,6 +62,55 @@ class _EditProductScreenState extends State<EditProductScreen> {
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
               focusNode: _priceFocusNode,
+              onFieldSubmitted: (_) {
+                print(
+                  _imageUrlController.text,
+                );
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Description'),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  height: 100,
+                  margin: EdgeInsets.only(
+                    top: 8,
+                    right: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  child: _imageUrlController.text.isEmpty
+                      ? Text('Enter a URL')
+                      : FittedBox(
+                          child: Image.network(
+                            _imageUrlController.text.trim(),
+                            //  'https://media.gettyimages.com/photos/cheesy-pepperoni-pizza-picture-id938742222?s=612x612',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(labelText: 'Image URL'),
+                    keyboardType: TextInputType.url,
+                    textInputAction: TextInputAction.done,
+                    controller: _imageUrlController,
+                    focusNode: _imageUrlFocusNode,
+                  ),
+                )
+              ],
             )
           ],
         ),
